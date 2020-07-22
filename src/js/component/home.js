136be9f8fb829f115d37ed68d6c0818a78724f14
sergioadll <sergio.adll@hotmail.com>
2020-07-22 16:01:43 +0000

    first-commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,40 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export default class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = { activeLight: null };
+	}
+	render() {
+		console.log(this.state);
+		return (
+			<div className="">
+				<div className="trafficPole mx-auto" />
+				<div className="rounded mx-auto py-2 trafficLight">
+					<div
+						className={
+							"bg-success rounded-circle mx-auto my-1 light " +
+							this.state
+						}
+						onClick={() => this.setState({ activeLight: "green" })}
+					/>
+					<div
+						className={
+							"bg-warning rounded-circle mx-auto my-1 light " +
+							this.state
+						}
+						onClick={() => this.setState({ activeLight: "yellow" })}
+					/>
+					<div
+						className={
+							"bg-danger rounded-circle mx-auto my-1 light " +
+							this.state
+						}
+						onClick={() => this.setState({ activeLight: "red" })}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
